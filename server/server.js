1654bcd8d569d58a50c0cec53169cee8d3aea3f1
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-04-02 05:27:31 +0530

    env addition

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors')
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
 
 const app = express();
 app.use(cors());
-const port = 3001;
+const port = PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
